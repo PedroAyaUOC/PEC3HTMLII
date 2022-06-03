1924ded6e40df61760b790b5c4a44917cfa8f9ef
“Pedro <payalalo@uoc.edu>
2022-06-03 13:17:25 -0700

    Primera Versión Producción

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      'purple': '#6f4980',
+      'purpleHov': '#a16096',
+      'navy': '#303667',
+      'white': '#ffffff',
+      'purpleGrad': 'linear-gradient(45deg, #303667 0%, #6f4980 52%, #a16096 100%)',
+      'neutral': '#171717',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
